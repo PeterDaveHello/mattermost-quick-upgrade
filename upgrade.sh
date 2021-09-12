9c65ef5601447bfaba23af7d9786075721ad1e5c
Peter Dave Hello <hsu@peterdavehello.org>
2021-09-12 17:56:16 +0800

    Add some human readable and colorful output

--- upgrade.sh
+++ upgrade.sh
@@ -2,10 +2,21 @@
 
 {
 
+  # ColorEchoForShell
+  # https://github.com/PeterDaveHello/ColorEchoForShell
+
+  echoCyan() {
+    echo "\\033[36m$*\\033[m"
+  }
+
+  echoRed() {
+    echo "\\033[31m$*\\033[m"
+  }
+
   set -ex
 
   error() {
-    >&2 echo "$@"
+    echoRed >&2 "$@"
     exit 1
   }
 
@@ -13,6 +24,7 @@
     error "Please give me root permission"
   fi
 
+  echoCyan "Setting Mattermost version and install path"
   MATTERMOST_VERSION="${MATTERMOST_VERSION:-5.38.2}"
   INSTALL_PATH="${INSTALL_PATH:-/opt}"
 
@@ -20,6 +32,7 @@
     error "${INSTALL_PATH} doesn't seem to be the right path!"
   fi
 
+  echoCyan "Preparing working directory and install package"
   TMP="$(mktemp -d --suffix=-mattermost)"
 
   if ! cd "${TMP}"; then
@@ -32,20 +45,28 @@
 
   chown -hR mattermost:mattermost ./mattermost-upgrade/
 
+  echoCyan "Stopping Mattermost service"
   systemctl stop mattermost
 
+  echoCyan "Making backup of current Mattermost files"
   cp -ra "${INSTALL_PATH}/mattermost/" "${INSTALL_PATH}/mattermost-back-$(date +'%F-%H-%M')/"
 
   if ! cd "${INSTALL_PATH}"; then
     error "Change directory to ${INSTALL_PATH} failed"
   fi
 
+  echoCyan "Clearing up Mattermost directory"
   find mattermost/ mattermost/client/ -mindepth 1 -maxdepth 1 \! \( -type d \( -path mattermost/client -o -path mattermost/client/plugins -o -path mattermost/config -o -path mattermost/logs -o -path mattermost/plugins -o -path mattermost/data -o -path mattermost/yourFolderHere \) -prune \) | sort | xargs rm -r
 
+  echoCyan "Copy new files into Mattermost path"
   cp -an "${TMP}/mattermost-upgrade/." "${INSTALL_PATH}/mattermost/"
 
+  echoCyan "Starting Mattermost service"
   systemctl start mattermost
 
+  echoCyan "Cleaning up temporary working direcroty"
   rm -rf "${TMP}"
 
+  echoCyan "Upgrade process is finished!"
+  echoCyan "Please give it a few seconds to finish the migration and bootstrap processbootstrap process.."
 }
